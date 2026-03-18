f0b3608d448f3a675b867b3ec02bfb4ee123ef9e
vivcis <ceciliaorji.co@gmail.com>
2026-03-18 12:44:29 +0100

    update 05.sh command to create walet, fund it and get bal

--- submission/05.sh
+++ submission/05.sh
@@ -1,4 +1,17 @@
 # Check the total amount in the wallet.
 
 #!/bin/bash
-bitcoin-cli -regtest getbalance
+#bitcoin-cli -regtest getbalance
+
+bitcoin-cli -regtest createwallet "testwalletbalance"
+
+# Generate an address in the wallet
+ADDRESS=$(bitcoin-cli -regtest -rpcwallet=testwalletbalance getnewaddress)
+
+# Mine 101 blocks to fund the wallet (first 100 are unspendable, 101st is spendable)
+bitcoin-cli -regtest generatetoaddress 101 $ADDRESS
+
+# Check the balance
+BALANCE=$(bitcoin-cli -regtest -rpcwallet=testwalletbalance getbalance)
+
+echo $BALANCE

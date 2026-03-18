66b9889508e664d92c40abb4120a679ba408478c
vivcis <ceciliaorji.co@gmail.com>
2026-03-18 13:04:56 +0100

    fix

--- submission/06.sh
+++ submission/06.sh
@@ -4,7 +4,7 @@
 #bitcoin-cli -regtest getnewaddress "" legacy
 
 # Create wallet if it doesn't exist
-bitcoin-cli -regtest createwallet "legacywallet" 2>/dev/null || true
+bitcoin-cli -regtest createwallet "legacywallet" > /dev/null 2>&1 || true
 
 # Generate a legacy address and output only the address
 bitcoin-cli -regtest -rpcwallet=legacywallet getnewaddress "" legacy
